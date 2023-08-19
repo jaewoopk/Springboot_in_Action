12a4c33ace097f6ae19576a6f278a023bd92ee2f
jaewoopk <jaewoopk000@naver.com>
2023-08-19 13:30:16 +0900

    Day 4 Learn about Validations, Hybernate, Process Methods, to link several MVC

--- taco-clould/src/main/java/tacos/web/DesignTacoController.java
+++ taco-clould/src/main/java/tacos/web/DesignTacoController.java
@@ -1,8 +1,10 @@
 package tacos.web;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,10 +56,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         // 이 지점에서 타코 디자인을 저장한다.
         log.info("Processing design: " + design);
-
         return "redirect:/orders/current";
     }
 }
