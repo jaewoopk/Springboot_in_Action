3cd6659ec622fcbf8403123fdc74c302edec833d
jaewoopk <jaewoopk000@naver.com>
2023-08-28 17:58:02 +0900

    Day 8 Learn about implementations, how to work but failed to load h2-database

--- taco-clould/src/main/java/tacos/data/JdbcOrderRepository.java
+++ taco-clould/src/main/java/tacos/data/JdbcOrderRepository.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Repository;
 import tacos.Taco;
 import tacos.Order;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class JdbcOrderRepository implements OrderRepository{
     private SimpleJdbcInsert orderInserter;
@@ -28,6 +33,35 @@ public class JdbcOrderRepository implements OrderRepository{
 
     @Override
     public Order save(Order order) {
-        return null;
+        order.setPlacedAt(new Date());
+        long orderId = saveOrderDetails(order);
+        order.setId(orderId);
+        List<Taco> tacos = order.getTacos();
+
+        for (Taco taco : tacos) {
+            saveTacoToOrder(taco, orderId);
+        }
+
+        return order;
+    }
+
+    private long saveOrderDetails(Order order) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> values =
+                objectMapper.convertValue(order, Map.class);
+        values.put("placedAt", order.getPlacedAt());
+
+        long orderId =
+                orderInserter
+                        .executeAndReturnKey(values)
+                        .longValue();
+        return orderId;
+    }
+
+    private void saveTacoToOrder(Taco taco, long orderId) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("tacoOrder", orderId);
+        values.put("taco", taco.getId());
+        orderTacoInserter.execute(values);
     }
 }
