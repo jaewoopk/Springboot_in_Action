6d2bf46a00b3ad3078f4338a8213e8892be5c072
jaewoopk <jaewoopk000@naver.com>
2023-08-30 17:52:02 +0900

    Day 10 Learn about differences between Jdbc and JPA, transition to JPA

--- taco-clould/src/main/java/tacos/Taco.java
+++ taco-clould/src/main/java/tacos/Taco.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,13 +9,24 @@ import java.util.List;
 import java.util.Date;
 
 @Data
+@Entity
 public class Taco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date createdAt;
+
     @NotNull
     @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
